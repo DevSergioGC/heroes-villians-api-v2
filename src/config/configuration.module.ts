412061718e3b2dc13cada685fc66f12b836f6930
Sergio Guzmán <82333906+DevSergioGC@users.noreply.github.com>
2024-06-01 22:17:30 -0400

    add a jwtConfigService for the jwt secret

--- src/config/configuration.module.ts
+++ src/config/configuration.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseConfigService } from './index.service';
 import { CryptoConfigService } from './crypto-config.service';
+import { JwtConfigService } from './jwt-config.service';
 
 @Module({
   imports: [],
-  providers: [DatabaseConfigService, CryptoConfigService],
-  exports: [DatabaseConfigService, CryptoConfigService],
+  providers: [DatabaseConfigService, CryptoConfigService, JwtConfigService],
+  exports: [DatabaseConfigService, CryptoConfigService, JwtConfigService],
 })
 export class ConfigurationModule {}
--- src/config/index.service.ts
+++ src/config/index.service.ts
@@ -1,2 +1,3 @@
 export { DatabaseConfigService } from './database-config.service';
 export { CryptoConfigService } from './crypto-config.service';
+export { JwtConfigService } from './jwt-config.service';
