6c98c67fe3b6a4605c68965b2397c9ed8c61dc42
Sergio Guzmán <82333906+DevSergioGC@users.noreply.github.com>
2024-06-02 22:27:03 -0400

    add a global guard & add custom decorator
    Implement a custom decorator for routes that dont need authroization

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './exception-filters/http.exception-filter';
+import { JwtAuthGuard } from './modules/auth/guards/jwt.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalGuards(new JwtAuthGuard(app.get(Reflector)));
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { LocalGuard } from './guards/local.guard';
+import { SkipGuard } from 'src/decorators/jwt-exceptions.decorator';
 
 @Controller()
 export class AuthController {
   @Post('login')
+  @SkipGuard()
   @UseGuards(LocalGuard)
   async login(@Req() req: Request) {
     return { token: req.user };
